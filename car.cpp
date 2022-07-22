c174987dabb4070aac4934e90d2c145b7c3abda1
Taha <tcollegewala30@gmail.com>
2022-07-22 11:43:52 +0530

    Inital commit

---
+++ car.cpp
@@ -0,0 +1,15 @@
+#include<graphics.h>
+int main(){
+    int gd=DETECT,gm;
+    initgraph(&gd, &gm, (char*)"");
+    bar(150,250,450,300);
+    bar(220,150,390,250);
+    arc(200,325,0,360,25);
+    arc(410,325,0,360,25);
+    bar(200,220,200,220);
+    arc(200,325,0,360,12);
+    arc(410,325,0,360,12);
+    getch();
+    closegraph();
+    return 0;
+} 
